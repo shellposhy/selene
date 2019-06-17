c51b8714d2d568b52fcef31255766946af52c789
shishaobo <shishaobo@zgshfp.com.cn>
2019-06-17 10:32:22 +0800

    add selene dataing provider modules

---
+++ selene-common/src/main/java/com/selene/common/Result.java
@@ -0,0 +1,32 @@
+package com.selene.common;
+
+import java.io.Serializable;
+
+/**
+ * Result {@code Result} information for response
+ * 
+ * @author shaobo shih
+ * @version 1.0
+ */
+public class Result implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String code;
+	private String msg;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+}
---
+++ selene-dataing/selene-dataing-provider/src/main/java/com/selene/dataing/provider/config/PropertyPlaceholderConfigurerExtension.java
@@ -0,0 +1,39 @@
+package com.selene.dataing.provider.config;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import static com.selene.common.util.IPs.local;
+import static cn.com.lemon.base.Strings.isNullOrEmpty;
+
+/**
+ * Modify the properties file contents before starting the service.
+ * 
+ * @author shellpo shih
+ * @version 1.0
+ */
+public class PropertyPlaceholderConfigurerExtension extends PropertyPlaceholderConfigurer {
+	private static final Logger LOG = LoggerFactory.getLogger(PropertyPlaceholderConfigurerExtension.class.getName());
+	private static final String SERVER_IP_AND_PORT_KEY = "hts.server.address";
+
+	@Override
+	protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)
+			throws BeansException {
+		String serverIPAndPort = props.getProperty(SERVER_IP_AND_PORT_KEY);
+		LOG.info("Old server address=" + serverIPAndPort);
+		String port = serverIPAndPort.split(":")[1];
+		String ip = local();
+		LOG.info("New server address=" + ip + ":" + port);
+		if (!isNullOrEmpty(serverIPAndPort)) {
+			// props.setProperty(SERVER_IP_AND_PORT_KEY, isNullOrEmpty(ip) ?
+			// serverIPAndPort : ip + ":" + port);
+			props.setProperty(SERVER_IP_AND_PORT_KEY, serverIPAndPort);
+		}
+		super.processProperties(beanFactoryToProcess, props);
+	}
+}
